d47f41cef7faef68133c0c57dff4f081f05b1466
shoukailiang <shoukailiang@qq.com>
2023-01-01 17:52:13 +0800

    初始化配置

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './components/App';
-import './index.less';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from '@/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement,
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+root.render(
+    <App />
+);
