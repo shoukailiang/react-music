aafe44abcef01b7a7649ed931ae4690dd68fe63c
shoukailiang <shoukailiang@qq.com>
2023-01-02 16:32:50 +0800

    配置less

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,19 @@
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
 const resolve = (dir) => path.resolve(__dirname, dir)
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true
+          }
+        }
+      }
+    }
+  ],
   // 配置别名
   webpack: {
     alias: {
