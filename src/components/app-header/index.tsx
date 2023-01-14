46c230dd64369afdff8b75a475f47860c9d54bdb
shoukailiang <shoukailiang@qq.com>
2023-01-14 22:18:03 +0800

    更新app-header

---
+++ src/components/app-header/index.tsx
@@ -0,0 +1,64 @@
+import React, { memo } from 'react'
+import type { ReactNode, FC } from 'react'
+import { NavLink } from 'react-router-dom'
+import { HeaderLeft, HeaderRight, HeaderWrapper } from './style'
+
+import headerTitles from '@/assets/data/header_titles.json'
+
+interface IProps {
+  children?: ReactNode
+}
+
+const AppHeader: FC<IProps> = () => {
+  /** 组件的展示逻辑 */
+  function showItem(item: any) {
+    if (item.type === 'path') {
+      return (
+        <NavLink
+          to={item.link}
+          className={({ isActive }) => {
+            return isActive ? 'active' : undefined
+          }}
+        >
+          {item.title}
+          <i className="icon sprite_01"></i>
+        </NavLink>
+      )
+    } else {
+      return (
+        <a href={item.link} rel="noreferrer" target="_blank">
+          {item.title}
+        </a>
+      )
+    }
+  }
+
+  return (
+    <HeaderWrapper>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a className="logo sprite_01" href="/">
+            哦哦啊音乐
+          </a>
+          <div className="title-list">
+            {headerTitles.map((item) => {
+              return (
+                <div className="item" key={item.title}>
+                  {showItem(item)}
+                </div>
+              )
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>
+          <span className="input">input</span>
+          <span className="center">创作者中心</span>
+          <span className="login">登录</span>
+        </HeaderRight>
+      </div>
+      <div className="divider"></div>
+    </HeaderWrapper>
+  )
+}
+
+export default memo(AppHeader)
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
 import 'normalize.css'
+
 import './assets/css/index.less'
+
 import App from '@/App'
-import store from './store'
+import { Provider } from 'react-redux/es/exports'
+import store from '@/store'
+import { ThemeProvider } from 'styled-components'
+import theme from './assets/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
   <Provider store={store}>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <ThemeProvider theme={theme}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ThemeProvider>
   </Provider>
 )
